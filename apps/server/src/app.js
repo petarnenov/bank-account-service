5c91b3f2933dc81c3fa98d21a45877463a4fb184
Petar Petrov <petarnenovpetrov@gmail.com>
2025-07-26 18:46:24 +0300

    Prepare for Heroku deployment: add Procfile, static serving, build client

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -32,6 +32,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/customers', authenticateToken, customerRoutes);
 app.use('/api/accounts', authenticateToken, accountRoutes);
 
+// Serve static files from React app in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
